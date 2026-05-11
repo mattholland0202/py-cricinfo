5c3ef1cae5477636c4378ffd234455cf091827d9
Matt <mattholland0202@gmail.com>
2026-05-11 20:57:12 +0100

    Use correct types for Career

--- changelog.md
+++ changelog.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.36]
+### Fixed
+- Use correct types in Career model
+- Missing `.gitattributes` file so GitHub ignores test files in language checking
+
 ## [0.0.35]
 ### Added
 - Parsing of a Player page to get Career stats
--- pycricinfo/models/source/pages/player.py
+++ pycricinfo/models/source/pages/player.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import AliasChoices, BaseModel, ConfigDict, Field
+from pydantic import AliasChoices, BaseModel, ConfigDict, Field, model_validator
 
 from pycricinfo.types import MatchTypeNames
 
@@ -8,40 +8,56 @@ from pycricinfo.types import MatchTypeNames
 class CareerStatsBaseModel(BaseModel):
     model_config = ConfigDict(validate_by_name=True, validate_by_alias=True)
 
+    @model_validator(mode="before")
+    @classmethod
+    def normalize_empty_values(cls, data: dict):
+        if not data:
+            return data
+
+        for key, value in data.items():
+            if isinstance(value, str) and value.strip() == "-":
+                data[key] = None
+
+        return data
+
 
 class CareerBattingFieldingRow(CareerStatsBaseModel):
     format: MatchTypeNames
-    matches: Optional[str] = Field(default=None, validation_alias="mat")
-    innings: Optional[str] = Field(default=None, validation_alias="inns")
-    not_outs: Optional[str] = Field(default=None, validation_alias="no")
-    runs: Optional[str] = Field(default=None)
-    highest_score: Optional[str] = Field(default=None, validation_alias="hs")
-    average: Optional[str] = Field(default=None, validation_alias="ave")
-    balls_faced: Optional[str] = Field(default=None, validation_alias="bf")
-    strike_rate: Optional[str] = Field(default=None, validation_alias="sr")
-    centuries: Optional[str] = Field(default=None, validation_alias=AliasChoices("100s", "hundreds"))
-    half_centuries: Optional[str] = Field(default=None, validation_alias=AliasChoices("50s", "fifties"))
-    fours: Optional[str] = Field(default=None, validation_alias="4s")
-    sixes: Optional[str] = Field(default=None, validation_alias="6s")
-    catches: Optional[str] = Field(default=None, validation_alias="ct")
-    stumpings: Optional[str] = Field(default=None, validation_alias="st")
+    matches: Optional[int] = Field(default=None, validation_alias="mat")
+    innings: Optional[int] = Field(default=None, validation_alias="inns")
+    not_outs: Optional[int] = Field(default=None, validation_alias="no")
+    runs: Optional[int] = Field(default=None)
+    highest_score: Optional[str] = Field(
+        default=None,
+        validation_alias="hs",
+        description="The highest career score, which is a string as it may include a '*' to indicate not out",
+    )
+    average: Optional[float] = Field(default=None, validation_alias="ave")
+    balls_faced: Optional[int] = Field(default=None, validation_alias="bf")
+    strike_rate: Optional[float] = Field(default=None, validation_alias="sr")
+    centuries: Optional[int] = Field(default=None, validation_alias=AliasChoices("100s", "hundreds"))
+    half_centuries: Optional[int] = Field(default=None, validation_alias=AliasChoices("50s", "fifties"))
+    fours: Optional[int] = Field(default=None, validation_alias="4s")
+    sixes: Optional[int] = Field(default=None, validation_alias="6s")
+    catches: Optional[int] = Field(default=None, validation_alias="ct")
+    stumpings: Optional[int] = Field(default=None, validation_alias="st")
 
 
 class CareerBowlingRow(CareerStatsBaseModel):
     format: MatchTypeNames
-    matches: Optional[str] = Field(default=None, validation_alias="mat")
-    innings: Optional[str] = Field(default=None, validation_alias="inns")
-    balls_bowled: Optional[str] = Field(default=None, validation_alias="balls")
-    runs_conceded: Optional[str] = Field(default=None, validation_alias="runs")
-    wickets: Optional[str] = Field(default=None, validation_alias="wkts")
+    matches: Optional[int] = Field(default=None, validation_alias="mat")
+    innings: Optional[int] = Field(default=None, validation_alias="inns")
+    balls_bowled: Optional[int] = Field(default=None, validation_alias="balls")
+    runs_conceded: Optional[int] = Field(default=None, validation_alias="runs")
+    wickets: Optional[int] = Field(default=None, validation_alias="wkts")
     best_bowling_innings: Optional[str] = Field(default=None, validation_alias="bbi")
     best_bowling_match: Optional[str] = Field(default=None, validation_alias="bbm")
-    average: Optional[str] = Field(default=None, validation_alias="ave")
-    economy_rate: Optional[str] = Field(default=None, validation_alias="econ")
-    strike_rate: Optional[str] = Field(default=None, validation_alias="sr")
-    four_wicket_hauls: Optional[str] = Field(default=None, validation_alias=AliasChoices("4w", "four_w"))
-    five_wicket_hauls: Optional[str] = Field(default=None, validation_alias=AliasChoices("5w", "five_w"))
-    ten_wicket_hauls: Optional[str] = Field(default=None, validation_alias=AliasChoices("10w", "ten_w"))
+    average: Optional[float] = Field(default=None, validation_alias="ave")
+    economy_rate: Optional[float] = Field(default=None, validation_alias="econ")
+    strike_rate: Optional[float] = Field(default=None, validation_alias="sr")
+    four_wicket_hauls: Optional[int] = Field(default=None, validation_alias=AliasChoices("4w", "four_w"))
+    five_wicket_hauls: Optional[int] = Field(default=None, validation_alias=AliasChoices("5w", "five_w"))
+    ten_wicket_hauls: Optional[int] = Field(default=None, validation_alias=AliasChoices("10w", "ten_w"))
 
 
 class Career(BaseModel):
